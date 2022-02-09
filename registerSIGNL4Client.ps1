a99ecf68eed092ee42532b9294697353042c4eac
René Bormann <rbormann@de.derdack.com>
2022-02-09 20:04:27 +0100

    Updated version

--- registerSIGNL4Client.ps1
+++ registerSIGNL4Client.ps1
@@ -1,7 +1,30 @@
-Import-Module Az.Resources # Imports the PSADPasswordCredential object
+# This script:
+# - Creates a new registered app in Azure AD (AzureAD)
+# - Adds a password credential to it (AzureAD)
+# - Creates a service principal for that app (Microsoft.Graph.Applications)
+# - Creates a new dedicated role for that service principal which has only access to Azure Sentinel stuff (Az)
+# - Assigns the service principal to that role (Az)
 
-$SIGNL4AppNameAzure = "SIGNL4AzureSentinelLogAnalyticsAPI"
-$SIGNL4AzureRoleName = "Access to Azure Sentinel and Log Analytics API for S4";
+# Tested date 02/09/2022
+# You'll need to auth to Azure with an Azure tenant amdin account multiple times because three different modules / tech stacks are used
+# If modules below are not installed in your environment use these commands:
+# Install-Module -Name Microsoft.Graph.Applications     # tested with 1.9.2
+# Install-Module -Name AzureAD                          # tested with 2.0.2.16
+# Install-Module -Name Az                               # tested with 7.2.0
+
+
+# #################################################################################
+# NOTE: After this script has completed you may need to 
+# - log in to Azure Portal
+# - navigate to AzureAD -> App registrations -> <createdApp> -> API permissions
+# - click the button 'Grant admin consent for <your tenant name>
+# #################################################################################
+
+
+$SIGNL4AppNameAzure = "AzureSentinel and LogAnalytics Client for SIGNL4"
+$SIGNL4AzureRoleName = "Access to Azure Sentinel and Log Analytics API for 3rd party systems";
+$SIGNL4AppIdentifierUri = "api://AzureSentinelandLogAnalyticsClientforSIGNL4"
+
 
 $s4config = [pscustomobject]@{
 SubscriptionId = ''
@@ -11,7 +34,10 @@ ClientSecret = ''
 }
 
 # Login to Azure
-Connect-AzAccount
+Connect-AzAccount #For PS Module 'Az'
+Connect-AzureAD #For PS Module 'AzureAD'
+Connect-MgGraph -Scope "Directory.AccessAsUser.All" #For PS Module 'Microsoft.Graph.Applications'
+
 
 # Read and display all subscriptions
 $subscriptions = Get-AzSubscription
@@ -19,9 +45,9 @@ $subscriptions | Format-Table -Property SubscriptionId,Name,State,TenantId
 
 $subIndex = Read-Host -Prompt "Please enter row number of subscription to use (starting from 1)"
 
+
 # Sets the tenant, subscription, and environment for cmdlets to use in the current session
 Set-AzContext -SubscriptionId $subscriptions[$subIndex-1].SubscriptionId
-
 
 $s4config.SubscriptionId = $subscriptions[$subIndex-1].SubscriptionId
 $s4config.TenantId = $subscriptions[$subIndex-1].TenantId
@@ -30,17 +56,38 @@ $s4config.TenantId = $subscriptions[$subIndex-1].TenantId
 $subScope = "/subscriptions/" + $s4config.SubscriptionId
 
 
-# Create the SPN in the sub
+# Create the App in the sub
+Write-Output "Creating a new Application for SIGNL4 in Azure AD..this will take 1 minute.."
+$app = New-AzureADApplication -DisplayName $SIGNL4AppNameAzure -IdentifierUris $SIGNL4AppIdentifierUri
+Start-Sleep -s 60 # Needed as it is otherwise not usable due to Azure APIU latency
+
+# Add an app password
+Write-Output "Adding a password to the SIGNL4 Application in Azure AD.."
 $spnPwd = New-Guid
-$credentials = New-Object Microsoft.Azure.Commands.ActiveDirectory.PSADPasswordCredential -Property @{ StartDate=Get-Date; EndDate=Get-Date -Year 2020; Password=$spnPwd}
-$spn = New-AzADServicePrincipal -DisplayName $SIGNL4AppNameAzure -PasswordCredential $credentials
+
+# Add required app API permissions for GraphAPI SecurityEvents/Alerts
+Write-Output "Adding required app API permissions for GraphAPI SecurityEvents/Alerts.." 
+Add-AzADAppPermission -ApiId '00000003-0000-0000-c000-000000000000' -PermissionId '34bf0e97-1971-4929-b999-9e2442d941d7' -ObjectId $app.ObjectId -Type 'Role'
+Add-AzADAppPermission -ApiId '00000003-0000-0000-c000-000000000000' -PermissionId '45cc0394-e837-488b-a098-1918f48d186c' -ObjectId $app.ObjectId -Type 'Role'
+Add-AzADAppPermission -ApiId '00000003-0000-0000-c000-000000000000' -PermissionId 'ed4fca05-be46-441f-9803-1873825f8fdb' -ObjectId $app.ObjectId -Type 'Role'
+Add-AzADAppPermission -ApiId '00000003-0000-0000-c000-000000000000' -PermissionId '472e4a4d-bb4a-4026-98d1-0b0d74cb74a5' -ObjectId $app.ObjectId -Type 'Role'
+Add-AzADAppPermission -ApiId '00000003-0000-0000-c000-000000000000' -PermissionId 'd903a879-88e0-4c09-b0c9-82f6a1333f84' -ObjectId $app.ObjectId -Type 'Role'
+Add-AzADAppPermission -ApiId '00000003-0000-0000-c000-000000000000' -PermissionId 'bf394140-e372-4bf9-a898-299cfc7564e5' -ObjectId $app.ObjectId -Type 'Role'
+
+### Create the SPN in the sub
+Write-Output "Creating an SPN for the SIGNL4 Application Azure AD.."
+$params = @{
+	AppId = $app.appId
+}
+$spn = New-MgServicePrincipal -BodyParameter $params
 
 
-Write-Output "SPN created in Azure:"
-$spn | Format-Table -Property ApplicationId,DisplayName,Id,ServicePrincipalNames
 
-$s4config.ClientId = $spn.ApplicationId
-$s4config.ClientSecret = $spnPwd #$spn.Secret | ConvertFrom-SecureString
+Write-Output "App and SPN created in Azure:"
+$spn | Format-Table -Property AppId,DisplayName,Id
+
+$s4config.ClientId = $spn.AppId
+$s4config.ClientSecret = $spnPwd
 
 
 
@@ -71,8 +118,10 @@ $role.AssignableScopes.Add($subScope)
 Write-Output "Creating new role in Azure, which may take some seconds..."
 New-AzRoleDefinition -Role $role
 
+
 # Sleep a little while and wait until the new role is completely populated and available in Azure. Otherwise consider adding the role assignment manually in Azure Portal. The SPN shows up for assignement..
-Start-Sleep -s 30
+Start-Sleep -s 60
+
 
 # Assign SPN to that role
 Write-Output "Role created in Azure, adding SPN to that role..."
@@ -81,7 +130,7 @@ New-AzRoleAssignment -ObjectId $spn.Id -RoleDefinitionName $SIGNL4AzureRoleName 
 Start-Sleep -s 10
 
 # Also add to Azure Sentinal Contributor role to be able to read Cases 
-New-AzRoleAssignment -ObjectId $spn.Id -RoleDefinitionName "Azure Sentinel Contributor" -Scope $subScope
+New-AzRoleAssignment -ObjectId $spn.Id -RoleDefinitionName "Microsoft Sentinel Contributor" -Scope $subScope
 
 
 
